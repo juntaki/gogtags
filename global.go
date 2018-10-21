d15a077121f215b2b8fdff67fe92e8e719fbbbfb
juntaki <me@juntaki.com>
2018-10-21 10:14:43 +0900

    Split

---
+++ global.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+type tagType int
+
+const (
+	GTAGS tagType = iota
+	GRTAGS
+	GPATH
+)
+
+func (t tagType) String() string {
+	switch t {
+	case GTAGS:
+		return "GTAGS"
+	case GRTAGS:
+		return "GRTAGS"
+	case GPATH:
+		return "GPATH"
+	}
+	panic("invalid tagType")
+}
+
+type standard struct {
+	tagName    string
+	fileID     int
+	lineNumber int
+	lineImage  string
+}
+
+func (s standard) String() string {
+	return fmt.Sprintf("%d @n %d %s", s.fileID, s.lineNumber, s.lineImage)
+}
+
+type compact struct {
+	fileID      int
+	lineNumbers []int
+}
+
+func (c compact) String() string {
+	continueCounter := 0
+	output := fmt.Sprintf("%d", c.lineNumbers[0]) // [0] must be exist
+	for l := 1; l < len(c.lineNumbers); l++ {
+		diff := c.lineNumbers[l] - c.lineNumbers[l-1]
+		if continueCounter == 0 {
+			if diff == 1 {
+				output += "-"
+				continueCounter++
+			} else {
+				output += fmt.Sprintf(",%d", diff)
+			}
+		} else {
+			if diff == 1 {
+				continueCounter++
+			} else {
+				output += fmt.Sprintf("%d,%d", continueCounter, diff)
+				continueCounter = 0
+			}
+		}
+	}
+	if continueCounter != 0 {
+		output += fmt.Sprintf("%d", continueCounter)
+	}
+	return fmt.Sprintf("%d @n %s", c.fileID, output)
+}
